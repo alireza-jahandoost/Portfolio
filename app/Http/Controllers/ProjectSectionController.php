5c6cacf2fe1d52ae1b813ffccd42476becd45109
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-03 15:53:33 +0430

    Create ProjectSection model with related classes

---
+++ app/Http/Controllers/ProjectSectionController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProjectSectionRequest;
+use App\Http\Requests\UpdateProjectSectionRequest;
+use App\Models\ProjectSection;
+
+class ProjectSectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProjectSectionRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProjectSectionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProjectSection  $projectSection
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProjectSection $projectSection)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProjectSection  $projectSection
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProjectSection $projectSection)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProjectSectionRequest  $request
+     * @param  \App\Models\ProjectSection  $projectSection
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProjectSectionRequest $request, ProjectSection $projectSection)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProjectSection  $projectSection
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProjectSection $projectSection)
+    {
+        //
+    }
+}
