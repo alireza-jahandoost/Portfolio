c5bc86f9e4541c297cfba1922bd709525aa29c5d
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-03 15:59:01 +0430

    Create ProjectImage model with related classes

---
+++ app/Http/Controllers/ProjectImageController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProjectImageRequest;
+use App\Http\Requests\UpdateProjectImageRequest;
+use App\Models\ProjectImage;
+
+class ProjectImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProjectImageRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProjectImageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProjectImage  $projectImage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProjectImage $projectImage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProjectImage  $projectImage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProjectImage $projectImage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProjectImageRequest  $request
+     * @param  \App\Models\ProjectImage  $projectImage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProjectImageRequest $request, ProjectImage $projectImage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProjectImage  $projectImage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProjectImage $projectImage)
+    {
+        //
+    }
+}
