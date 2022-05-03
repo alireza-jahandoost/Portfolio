217736a08774315e1b874234dc0c8a4cc093070b
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-03 19:04:23 +0430

    Install flowbite

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createInertiaApp } from '@inertiajs/inertia-react';
 import { InertiaProgress } from '@inertiajs/progress';
+import 'flowbite';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
