d26ad91d89fec977fd9e1cdae110104bf14c08e0
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-03 16:02:57 +0430

    Add necessary columns of landing_pages table

--- database/migrations/2022_05_03_112827_create_landing_pages_table.php
+++ database/migrations/2022_05_03_112827_create_landing_pages_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('landing_pages', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
             $table->timestamps();
         });
     }
