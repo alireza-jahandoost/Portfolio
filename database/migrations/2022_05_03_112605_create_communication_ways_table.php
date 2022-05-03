6146e95420a53ef0cc404d7c767f4b0e7f5afbea
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-03 16:03:59 +0430

    Add necessary columns of communication_ways table

--- database/migrations/2022_05_03_112605_create_communication_ways_table.php
+++ database/migrations/2022_05_03_112605_create_communication_ways_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('communication_ways', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('value');
+            $table->string('icon_class');
             $table->timestamps();
         });
     }
