ed7828019181139572996b7f5c7aea90705e7146
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-03 18:45:58 +0430

    Create landing page model automatically

--- app/Models/LandingPage.php
+++ app/Models/LandingPage.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed|string $description
+ * @property mixed|string $title
+ */
 class LandingPage extends Model
 {
     use HasFactory;
