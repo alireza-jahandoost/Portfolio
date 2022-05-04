d28e3c27f43ad6a731e35f5b30226d37ed0220b8
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-04 07:33:09 +0430

    Move general styles to Authenticated Layout

--- resources/js/Layouts/Authenticated.js
+++ resources/js/Layouts/Authenticated.js
@@ -21,7 +21,15 @@ export default function Authenticated({ auth, header, children }) {
                 </header>
             )}
 
-            <main>{children}</main>
+            <main>
+                <div className="py-12">
+                    <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            {children}
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
