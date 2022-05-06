22f6612dd9a8dd48da6e451d464b3e7e89a89b66
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-06 10:55:22 +0430

    Implement UserController/update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateUserProfileRequest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -16,5 +17,21 @@ class UserController extends Controller
     public function edit(): Response
     {
         return Inertia::render('AdminPanel/User/Edit');
+    }
+
+    public function update(UpdateUserProfileRequest $request){
+        $data  = $request->validated();
+
+        auth()->user()->name = $data['name'];
+        auth()->user()->email = $data['email'];
+        auth()->user()->about_me = $data['about_me'];
+
+        if(isset($data['image'])){
+            auth()->user()->image = $request->file('image')->store('profileImages');
+        }
+
+        auth()->user()->save();
+
+        return back();
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->boolean('is_admin')->default(false);
+            $table->string('image')->nullable();
             $table->string('password');
             $table->text('about_me');
             $table->rememberToken();
