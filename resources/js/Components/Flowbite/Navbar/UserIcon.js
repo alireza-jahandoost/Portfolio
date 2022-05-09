8073190148c03fa091811beb972f4cb5f7896036
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-09 11:09:11 +0430

    Remove unnecessary links in navbar dropdown

--- resources/js/Components/Flowbite/Navbar/UserIcon.js
+++ resources/js/Components/Flowbite/Navbar/UserIcon.js
@@ -87,14 +87,6 @@ const UserIcon = ({ auth }) => {
                             href="#"
                             className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                         >
-                            Earnings
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="#"
-                            className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                        >
                             Sign out
                         </a>
                     </li>
