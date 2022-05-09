4019d7664573a546a9030fbdde44cd78ef13a533
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-09 11:13:43 +0430

    Fix style of logout button in navbar dropdown

--- resources/js/Components/Flowbite/Navbar/UserIcon.js
+++ resources/js/Components/Flowbite/Navbar/UserIcon.js
@@ -88,7 +88,7 @@ const UserIcon = ({ auth }) => {
                             as="button"
                             method="POST"
                             href={route("logout")}
-                            className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                            className="block w-full text-left py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                         >
                             Sign out
                         </Link>
