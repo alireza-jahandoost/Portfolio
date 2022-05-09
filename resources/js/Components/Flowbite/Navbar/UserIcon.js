c1f984b0e01026a0bc0cb1b2d2f43a02870f2a2b
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-09 11:11:42 +0430

    Configure logout feature

--- resources/js/Components/Flowbite/Navbar/UserIcon.js
+++ resources/js/Components/Flowbite/Navbar/UserIcon.js
@@ -84,12 +84,14 @@ const UserIcon = ({ auth }) => {
                         </a>
                     </li>
                     <li>
-                        <a
-                            href="#"
+                        <Link
+                            as="button"
+                            method="POST"
+                            href={route("logout")}
                             className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                         >
                             Sign out
-                        </a>
+                        </Link>
                     </li>
                 </ul>
                 <div ref={setArrowElement} style={styles.arrow} />
