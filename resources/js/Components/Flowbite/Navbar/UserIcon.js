d2a81ad6f559cdfa987e373159bcdd329f81de2b
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-09 11:16:16 +0430

    Show user image in navbar

--- resources/js/Components/Flowbite/Navbar/UserIcon.js
+++ resources/js/Components/Flowbite/Navbar/UserIcon.js
@@ -37,18 +37,26 @@ const UserIcon = ({ auth }) => {
             >
                 <span className="sr-only">Open user menu</span>
                 <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                    <svg
-                        className="absolute w-12 h-12 text-gray-400 -left-1"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                            clipRule="evenodd"
+                    {auth.user.image ? (
+                        <img
+                            className="w-full h-full"
+                            src={`/${auth.user.image}`}
+                            alt="current user image"
                         />
-                    </svg>
+                    ) : (
+                        <svg
+                            className="absolute w-12 h-12 text-gray-400 -left-1"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    )}
                 </div>
             </button>
             <div
