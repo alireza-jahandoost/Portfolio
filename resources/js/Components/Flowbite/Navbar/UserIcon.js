b14993827776f2d20bca5c2189b4916da6a1cd5b
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-09 12:08:52 +0430

    Add link of settings to navbar dropdown

--- resources/js/Components/Flowbite/Navbar/UserIcon.js
+++ resources/js/Components/Flowbite/Navbar/UserIcon.js
@@ -84,12 +84,12 @@ const UserIcon = ({ auth }) => {
                         </Link>
                     </li>
                     <li>
-                        <a
-                            href="#"
+                        <Link
+                            href={route("admin.settings")}
                             className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                         >
                             Settings
-                        </a>
+                        </Link>
                     </li>
                     <li>
                         <Link
