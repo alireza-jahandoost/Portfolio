839e507633dbf7606591fd7ac49591323c84ede2
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-24 09:05:11 +0430

    Change hero text of header

--- resources/views/partials/landing/header.blade.php
+++ resources/views/partials/landing/header.blade.php
@@ -2,6 +2,6 @@
 background: radial-gradient(circle, rgba(40,40,40,1) 0%, rgba(0,0,0,1) 100%); ">
     <div class="flex flex-col justify-center h-full gap-16">
         <h2 class="text-white text-4xl sm:text-5xl lg:text-7xl text-center">Hello, I'm Alireza<br/> Jahandoost</h2>
-        <p class="text-white text-center text-xl sm:text-2xl lg:text-3xl">A middle level fullstack developer</p>
+        <p class="text-white text-center text-xl sm:text-2xl lg:text-3xl">A middle level fullstack web developer</p>
     </div>
 </div>
