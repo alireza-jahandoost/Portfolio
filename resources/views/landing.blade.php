8227543771160208dc73a8b6cd6bf9056dafe1c0
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-05-24 12:09:47 +0430

    Increase the space between main parts - landing

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -30,7 +30,7 @@
 <body class="antialiased">
 <div class="grid gap-y-8 mb-20 md:mb-0">
     @include('partials.general.navbar')
-    <div class="w-full max-w-screen-2xl mx-auto md:px-4 grid gap-12">
+    <div class="w-full max-w-screen-2xl mx-auto md:px-4 grid gap-12 md:gap-y-16 lg:gap-y-20">
         @include('partials.landing.header')
         @include('partials.landing.aboutMe')
         @include('partials.landing.skills')
