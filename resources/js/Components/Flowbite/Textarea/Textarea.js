b0763a5c729b81711776661a0015e79b29e629c3
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-06 15:53:51 +0430

    Change styles of Textarea when error exists

--- resources/js/Components/Flowbite/Textarea/Textarea.js
+++ resources/js/Components/Flowbite/Textarea/Textarea.js
@@ -16,7 +16,11 @@ const Textarea = ({
         <>
             <label
                 htmlFor={id}
-                className={`block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 ${labelClassName}`}
+                className={`block mb-2 text-sm font-medium ${
+                    error
+                        ? "text-red-600 dark:text-red-500"
+                        : "text-gray-900 dark:text-gray-300"
+                } ${labelClassName}`}
             >
                 {label}
             </label>
@@ -25,7 +29,11 @@ const Textarea = ({
                 rows={rowsNumber}
                 name={name}
                 onChange={onChange}
-                className={`block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${textareaClassName}`}
+                className={`${
+                    error
+                        ? `placeholder-red-400 bg-red-50 border-red-300 text-red-600 focus:ring-red-500 focus:border-red-500 dark:bg-red-700 dark:border-red-600 dark:placeholder-red-400 dark:text-white dark:focus:border-red-500 dark:focus:ring-red-500`
+                        : `bg-gray-50 border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 dark:focus:ring-red-500`
+                } border text-sm rounded-lg block w-full p-2.5 ${textareaClassName}`}
                 placeholder="Your message..."
                 value={value}
             />
