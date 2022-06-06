0b3ee699687d463e55f9be8d5821af4a86ee18d4
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-06 16:33:58 +0430

    Change styles of Select.js if error exists

--- resources/js/Components/Flowbite/Select/Select.js
+++ resources/js/Components/Flowbite/Select/Select.js
@@ -16,7 +16,11 @@ const Select = ({
         <>
             <label
                 htmlFor={id}
-                className={`block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400 ${labelClassName}`}
+                className={`block mb-2 text-sm font-medium ${
+                    error
+                        ? "text-red-600 dark:text-red-500"
+                        : "text-gray-900 dark:text-gray-300"
+                } ${labelClassName}`}
             >
                 {label}
             </label>
@@ -25,7 +29,11 @@ const Select = ({
                 onChange={onChange}
                 name={name}
                 id={id}
-                className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${selectClassName}`}
+                className={`${
+                    error
+                        ? `bg-red-50 border-red-300 text-red-600 focus:ring-red-500 focus:border-red-500 dark:bg-red-700 dark:border-red-600 dark:placeholder-red-400 dark:text-red-100 dark:focus:ring-red-500 dark:focus:border-red-500`
+                        : `bg-gray-50 border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`
+                } border text-sm rounded-lg block w-full p-2.5 ${selectClassName}`}
             >
                 {children}
             </select>
