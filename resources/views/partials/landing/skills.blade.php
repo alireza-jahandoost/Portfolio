360d5dbbb415d1728def4c680bc1a05b256af25b
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-06 13:56:03 +0430

    Link skills and projects sections to project page

--- resources/views/partials/landing/skills.blade.php
+++ resources/views/partials/landing/skills.blade.php
@@ -3,16 +3,18 @@
     <div class="lg:w-10/12 xl:w-9/12 mx-auto">
         <div class="flex flex-wrap gap-4 justify-center mt-6">
             @foreach($skills as $skill)
-                <div class="bg-gray-600 text-white px-4 py-2 rounded-lg flex flex-col">
-                    <div
-                        class="text-xs text-gray-200 md:text-sm lg:text-md">
-                        {{$skill->is_fluent?"Fluent in" : "Familiar with"}}
+                <a href="{{route('main.projects', ['skills' => [$skill->id]])}}">
+                    <div class="bg-gray-600 text-white px-4 py-2 rounded-lg flex flex-col">
+                        <div
+                            class="text-xs text-gray-200 md:text-sm lg:text-md">
+                            {{$skill->is_fluent?"Fluent in" : "Familiar with"}}
+                        </div>
+                        <div class="flex gap-2 items-baseline">
+                            <div class="text-md sm:text-lg md:text-xl lg:text-2xl">{{$skill->name}}</div>
+                            <div class="text-sm">{{$skill->projects_count}}</div>
+                        </div>
                     </div>
-                    <div class="flex gap-2 items-baseline">
-                        <div class="text-md sm:text-lg md:text-xl lg:text-2xl">{{$skill->name}}</div>
-                        <div class="text-sm">{{$skill->projects_count}}</div>
-                    </div>
-                </div>
+                </a>
             @endforeach
         </div>
     </div>
