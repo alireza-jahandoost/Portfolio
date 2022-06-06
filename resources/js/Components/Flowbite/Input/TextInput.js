d1402ad9dbdfdcf876d86242c016674825fc51ef
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-06 15:44:47 +0430

    Change styles of TextInput when error exists

--- resources/js/Components/Flowbite/Input/TextInput.js
+++ resources/js/Components/Flowbite/Input/TextInput.js
@@ -18,14 +18,22 @@ const TextInput = ({
         <>
             <label
                 htmlFor={id}
-                className={`block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 ${labelClassName}`}
+                className={`block mb-2 text-sm font-medium ${
+                    error
+                        ? "text-red-600 dark:text-red-500"
+                        : "text-gray-900 dark:text-gray-300"
+                } ${labelClassName}`}
             >
                 {label}
             </label>
             <input
                 type={type}
                 id={id}
-                className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${inputClassName}`}
+                className={`${
+                    error
+                        ? `placeholder-red-400 bg-red-50 border-red-300 text-red-600 focus:ring-red-500 focus:border-red-500 dark:bg-red-700 dark:border-red-600 dark:placeholder-red-400 dark:text-white dark:focus:border-red-500 dark:focus:ring-red-500`
+                        : `bg-gray-50 border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 dark:focus:ring-red-500`
+                } border text-sm rounded-lg block w-full p-2.5 ${inputClassName}`}
                 placeholder={placeholder}
                 name={name}
                 value={value}
