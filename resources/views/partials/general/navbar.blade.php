ca05dac63b57d0c9060e3efcf5ccc9c034a200ab
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-06 12:49:09 +0430

    Change navbar links

--- resources/views/partials/general/navbar.blade.php
+++ resources/views/partials/general/navbar.blade.php
@@ -2,22 +2,13 @@
     <div class="max-w-screen-2xl mx-auto px-4 hidden md:block">
         <div class="flex items-center flex-wrap gap-8 lg:gap-12 text-gray-700 my-4 px-2">
             <div class="hover:text-gray-800">
-                <a href="#">About Me</a>
+                <a href="{{route('main.landing_page')}}">Home</a>
             </div>
             <div class="hover:text-gray-800">
-                <a href="#">My Skills</a>
-            </div>
-            <div class="hover:text-gray-800">
-                <a href="#">My Projects</a>
-            </div>
-            <div class="hover:text-gray-800">
-                <a href="#">My Honors</a>
-            </div>
-            <div class="hover:text-gray-800">
-                <a href="#">Contact Me</a>
+                <a href="{{route('main.projects')}}">Projects</a>
             </div>
             <div class="ml-auto hover:text-gray-800 text-lg">
-                <a href="#">ARJHD</a>
+                Alireza Jahandoost
             </div>
         </div>
     </div>
