5f8f4494bc4450077a2d8a76bd28570df835ae05
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-08 17:58:07 +0430

    Change directory of partials of pages

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -2,11 +2,11 @@
     <div class="grid gap-y-8">
         @include('partials.general.navbar')
         <div class="w-full max-w-screen-2xl mx-auto md:px-4 grid gap-12 md:gap-y-16 lg:gap-y-20">
-            @include('partials.landing.header')
-            @include('partials.landing.aboutMe')
-            @include('partials.landing.skills')
-            @include('partials.landing.projectSections')
+            @include('partials.pages.landing.header')
+            @include('partials.pages.landing.aboutMe')
+            @include('partials.pages.landing.skills')
+            @include('partials.pages.landing.projectSections')
         </div>
-        @include('partials.landing.contactUs')
+        @include('partials.pages.landing.contactUs')
     </div>
 </x-layouts.main>
--- resources/views/pages/projects/index.blade.php
+++ resources/views/pages/projects/index.blade.php
@@ -14,8 +14,8 @@
     <div class="grid gap-y-8">
         @include('partials.general.navbar')
         <div class="w-full max-w-screen-2xl mx-auto md:px-4 grid gap-12 md:gap-y-16 lg:gap-y-20">
-            @include('partials.projects.header')
-            @include('partials.projects.projects')
+            @include('partials.pages.projects.index.header')
+            @include('partials.pages.projects.index.projects')
         </div>
     </div>
 
