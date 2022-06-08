dc2be654d763669c7df522f84ca261cebead3478
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-08 09:10:22 +0430

    Change style of Card.js

--- resources/js/Components/Personalized/Card/Card.js
+++ resources/js/Components/Personalized/Card/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ children, className, withoutPadding = false }) => {
     return (
         <div
-            className={`border-gray-100 border text-xl rounded-lg ${
+            className={`border-gray-100 border text-xl rounded-lg border shadow ${
                 !withoutPadding && "p-2"
             } text-gray-600 bg-white ${className}`}
         >
