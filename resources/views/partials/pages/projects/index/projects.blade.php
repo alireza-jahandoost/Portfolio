6cee44a5789d1587ee499a1adfa8994f63bf52ba
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-10 19:29:01 +0430

    Use semantic tags in projects.index page

--- resources/views/partials/pages/projects/index/projects.blade.php
+++ resources/views/partials/pages/projects/index/projects.blade.php
@@ -1,7 +1,9 @@
-<div class="grid md:grid-cols-2 lg:grid-cols-4 gap-y-8">
-    @foreach($projects as $project)
-        <div class="flex justify-center">
-            <x-project :project="$project"></x-project>
-        </div>
-    @endforeach
-</div>
+<section>
+    <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-y-8">
+        @foreach($projects as $project)
+            <div class="flex justify-center">
+                <x-project :project="$project"></x-project>
+            </div>
+        @endforeach
+    </div>
+</section>
