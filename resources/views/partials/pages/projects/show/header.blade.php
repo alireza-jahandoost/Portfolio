83578152551b1bf648fefc40e9b1411d6293f607
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-10 19:31:19 +0430

    Use semantic tags in projects.show page

--- resources/views/partials/pages/projects/show/header.blade.php
+++ resources/views/partials/pages/projects/show/header.blade.php
@@ -1,10 +1,14 @@
-<x-header-container>
-    <div>
-        <h2 class="text-white text-4xl sm:text-5xl lg:text-7xl text-center mb-28">{{$project->title}}</h2>
-        <div class="flex justify-center gap-3 flex-wrap w-11/12 mx-auto md:w-10/12 lg:w-9/12 xl:w-7/12">
-            @foreach($project->skills as $skill)
-                <div class="bg-gray-600 text-white p-2 rounded-md">{{$skill->name}}</div>
-            @endforeach
+<section>
+    <x-header-container>
+        <div>
+            <header>
+                <h1 class="text-white text-4xl sm:text-5xl lg:text-7xl text-center mb-28">{{$project->title}}</h1>
+            </header>
+            <div class="flex justify-center gap-3 flex-wrap w-11/12 mx-auto md:w-10/12 lg:w-9/12 xl:w-7/12">
+                @foreach($project->skills as $skill)
+                    <div class="bg-gray-600 text-white p-2 rounded-md">{{$skill->name}}</div>
+                @endforeach
+            </div>
         </div>
-    </div>
-</x-header-container>
+    </x-header-container>
+</section>
