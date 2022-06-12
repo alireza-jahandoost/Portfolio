29e79b474d4268d6503856d5bf52c7273f8885f7
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-12 13:00:17 +0430

    Share user variable along all views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,33 +2,37 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-/**
- * Register any application services.
- *
- * @return void
- */
-public function register()
-{
-    //
-}
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
-/**
- * Bootstrap any application services.
- *
- * @return void
- */
-public function boot()
-{
-Blade::directive('markdown', function ($expression) {
-    return "<?php echo Str::markdown($expression)?>";
-});
-Blade::directive('firstParagraph', function ($expression) {
-return "<?php echo Str::markdown(explode(PHP_EOL, $expression)[0]) ?>";
-});
-}
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::directive('markdown', function ($expression) {
+            return "<?php echo Str::markdown($expression)?>";
+        });
+        Blade::directive('firstParagraph', function ($expression) {
+            return "<?php echo Str::markdown(explode(PHP_EOL, $expression)[0]) ?>";
+        });
+
+        View::share('user', User::first());
+    }
 }
