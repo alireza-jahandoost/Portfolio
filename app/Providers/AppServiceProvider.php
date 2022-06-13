924103dc2ca941b2d372df7d305a94188c805b5a
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-13 09:35:35 +0430

    Specify default string length - AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+        
         Blade::directive('markdown', function ($expression) {
             return "<?php echo Str::markdown($expression)?>";
         });
