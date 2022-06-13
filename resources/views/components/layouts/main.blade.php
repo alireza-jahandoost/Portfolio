0439aef8334dd9f336421d3ce29245071e9a0f59
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-13 09:48:56 +0430

    Use mix in main layout

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Styles -->
 
-    <link href="/css/app.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
           integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
