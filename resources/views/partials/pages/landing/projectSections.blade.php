0bac192807cfd7ac1ddd2d03fe55002546a93a18
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-16 12:40:29 +0430

    Add badge to project sections

--- resources/views/partials/pages/landing/projectSections.blade.php
+++ resources/views/partials/pages/landing/projectSections.blade.php
@@ -5,7 +5,14 @@
             <div>
                 <header>
                     <a href="{{route('main.projects.index', ['project_section' => $projectSection->id])}}">
-                        <h2 class="text-center text-2xl underline font-bold">{{$projectSection->name}}</h2>
+                        <div class="flex justify-center items-center gap-2">
+                            <h2 class="text-center text-2xl underline font-bold">{{$projectSection->name}}</h2>
+                            @if(count($projectSection->projects)>2)
+                                <span
+                                    title="{{count($projectSection->projects)-2 . " more project(s) in this section"}}"
+                                    class="bg-gray-100 text-black text-sm font-semibold inline-flex items-center p-1.5 rounded-full max-w-min">+{{count($projectSection->projects)-2}}</span>
+                            @endif
+                        </div>
                     </a>
                 </header>
                 <div class="flex justify-center flex-wrap gap-6 md:gap-10 lg:gap-16 px-2 mt-8">
