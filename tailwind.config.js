227932fd9a1caa4412b68cd8abb2776a308d36eb
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-16 09:59:44 +0430

    Install @tailwindcss/typography
    Install @tailwindcss/typography for supporting
    mark downs styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.js',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.js",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
     plugins: [
-        require('@tailwindcss/forms'),
-        require('flowbite/plugin')
+        require("@tailwindcss/forms"),
+        require("flowbite/plugin"),
+        require("@tailwindcss/typography"),
     ],
 };
