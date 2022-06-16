2663a1cafebc0d1064f2d256e5ca018570ca4867
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-16 11:26:01 +0430

    Change spacings in landing/projectSections

--- resources/views/partials/pages/landing/projectSections.blade.php
+++ resources/views/partials/pages/landing/projectSections.blade.php
@@ -1,5 +1,5 @@
 <section>
-    <div class="w-full grid lg:grid-cols-2 md:px-24 lg:px-0 gap-2">
+    <div class="w-full grid lg:grid-cols-2 md:px-24 lg:px-0 gap-x-2 gap-y-12">
         @foreach($projectSections as $projectSection)
             @continue(count($projectSection->projects) === 0)
             <div>
@@ -8,7 +8,7 @@
                         <h2 class="text-center text-2xl underline font-bold">{{$projectSection->name}}</h2>
                     </a>
                 </header>
-                <div class="flex justify-around flex-wrap gap-6 px-2 mt-8">
+                <div class="flex justify-around flex-wrap gap-6 px-2 mt-6">
                     @foreach($projectSection->projects as $project)
                         @break($loop->iteration > 2)
                         <x-project :project="$project"></x-project>
