731a70778baab8268c41b863dce4ce84850acbcf
Alireza Jahandoost <Alireza.jhd2000@gmail.com>
2022-06-16 12:54:55 +0430

    Change style of skills

--- resources/views/partials/pages/landing/skills.blade.php
+++ resources/views/partials/pages/landing/skills.blade.php
@@ -4,10 +4,10 @@
             <h2 class="text-2xl lg:text-3xl font-bold text-center underline">My Skills</h2>
         </header>
         <div class="lg:w-10/12 xl:w-9/12 mx-auto">
-            <div class="flex flex-wrap gap-4 justify-center mt-6">
+            <div class="flex flex-wrap gap-4 justify-center mt-6 text-center">
                 @foreach($skills as $skill)
                     <a href="{{route('main.projects.index', ['skills' => [$skill->id]])}}">
-                        <div class="bg-gray-600 text-white px-4 py-2 rounded-lg flex flex-col">
+                        <div class="bg-gray-500 text-white px-8 py-2 rounded-lg flex flex-col">
                             <div
                                 class="text-xs text-gray-200 md:text-sm lg:text-md">
                                 {{$skill->is_fluent?"Fluent in" : "Familiar with"}}
